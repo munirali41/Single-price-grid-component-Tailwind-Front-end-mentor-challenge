908971b9e9fd5b747540bcd095bb06ca01a55336
Munir Ali <thecodingsheykh63@gmail.com>
2023-05-14 15:16:09 +0300

    Setup for Html and  layout (tailwind )

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0xBDCFED",
+      },
+      textColor: {
+        skin: {
+          head: "var(--color-text-head)",
+          span: "var(--color-text-span)",
+          butn: "var(--color-text-btn)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
